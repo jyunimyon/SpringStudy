65f87943a8e7d3dc47f4120f4da730492cfc2b62
Jiyun Kim <adela0603@kyonggi.ac.kr>
2022-11-14 01:58:38 +0900

    section_6

--- study/demo/demo/src/main/java/jyunyy/demo/controller/MemberController.java
+++ study/demo/demo/src/main/java/jyunyy/demo/controller/MemberController.java
@@ -18,4 +18,25 @@ public class MemberController {
 //    public void setMemberService(MemberService memberService){
 //        this.memberService=memberService;
 //    }
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+        //여기로 이동해라. 이거 실행되면 내가 template에 만들어둔 members/create어쩌구 여기로 이동하는거임
+        //여튼 createMemberForm.html이 뿌려지는거임
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member=new Member();
+        member.setName(form.getName());
+        //System.out.println("member="+member.getName());
+        memberService.join(member); //join을 통해 넣어주기
+
+        return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members=memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList"; //이거 이제 만들러 가야함 왜냐 여기로 이동해야되니까
+    }
 }
